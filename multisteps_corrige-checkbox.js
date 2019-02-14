cfb023c17d9e50d7b69510e63ee036c92062e210
hqbronzoni <hqbronzoni@gmail.com>
2019-02-14 17:35:03 -0200

    Corrige comportamento dos checkboxes
    Corrige comportamento dos checkboxes que entram marcados por padrão.

---
+++ multisteps_corrige-checkbox.js
@@ -0,0 +1,6 @@
+<script>
+     $(document).ready(function(){
+       document.getElementById("person_sms").checked = false;
+       document.getElementById("person_emails").checked = false;
+     })
+</script>
