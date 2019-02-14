dd3f65790b709a8a6e4d82f2221ab0fe5e7154d9
hqbronzoni <hqbronzoni@gmail.com>
2019-02-14 17:33:30 -0200

    Altera o texto no checkbox de opt-in

---
+++ multisteps_altera-optin.js
@@ -0,0 +1,5 @@
+<script>  
+     $(document).ready(function(){
+       document.getElementsByTagName("label")[7].innerHTML = "Estou de acordo com a <a class=\"modal_button\" data-target=\".service_term\" href=\"#\">Política de Doação</a> e a <br /><a class=\"modal_button\" data-target=\".privacy_policy\" href=\"#\">Politica de Privacidade</a> da Transparência Internacional.";
+     })
+</script>
