629968938a225acd1b6b160a9805f1e975c0d78b
hqbronzoni <hqbronzoni@gmail.com>
2019-02-14 17:48:15 -0200

    Altera o background com um vídeo

---
+++ onepage_alterabackground.js
@@ -0,0 +1,5 @@
+<script>  
+     $(document).ready(function(){
+       $("body").prepend("<video playsinline autoplay muted loop id=\"myVideo\"><source src=\"LINK-DO-VIDEO.MP4\" type=\"video/mp4\"><source src=\"LINK-DO-VIDEO.OGV\" type=\"video/ogv\"></video>");
+     })
+</script>
