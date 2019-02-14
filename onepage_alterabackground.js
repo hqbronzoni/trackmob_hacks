26214a1833eb05dc5ee4930b82570d5660c75e3c
hqbronzoni <hqbronzoni@gmail.com>
2019-02-14 17:52:31 -0200

    Insere vídeo no background da tela

--- onepage_alterabackground.js
+++ onepage_alterabackground.js
@@ -3,3 +3,17 @@
        $("body").prepend("<video playsinline autoplay muted loop id=\"myVideo\"><source src=\"LINK-DO-VIDEO.MP4\" type=\"video/mp4\"><source src=\"LINK-DO-VIDEO.OGV\" type=\"video/ogv\"></video>");
      })
 </script>
+
+<style>
+#myVideo {
+  position: fixed;
+  right: 0;
+  top: 0;
+  min-width: 100%;
+  min-height: 100%;
+}
+
+.main {
+position: relative;
+}
+</style>
