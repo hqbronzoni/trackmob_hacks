9e9991d196661b52f5cad750a2de21897c2fc9ef
hqbronzoni <hqbronzoni@gmail.com>
2019-02-14 17:40:59 -0200

    Consulta o e-mail salvo nos cookies
    Preenche o formulário do passo 2 com o e-mail informado no passo 1 e salvo nos cookies

---
+++ multisteps_cookies_consulta.js
@@ -0,0 +1,10 @@
+<!-- Inserir apenas no Passo 2 -->
+<script>
+     $(document).ready(function(){
+       document.getElementById("payment_custom_fields_emaildolead").value = getCookie("emailparard");
+     })
+  
+  	 $(document).ready(function(){
+       document.getElementsByClassName("payment_custom_fields_emaildolead")[0].setAttribute("class", "hide");
+     })
+</script>
