7d6a875eb3d92c7b9d634a0edd9aee8528a1d2cf
hqbronzoni <hqbronzoni@gmail.com>
2019-04-08 18:14:46 -0300

    Substitui símbolos de "passos" por selo do PayPal

---
+++ multisteps_insere-paypal.js
@@ -0,0 +1,6 @@
+<!-- PAYPAL E BITCOIN -->
+<script>
+     $(document).ready(function(){
+       document.querySelector(".steps").innerHTML = "<div style='width:100%'><div style='width:45%;float:left;'><p style='margin:25px 5px;'>Você também pode doar pelo PayPal:</p><p><a href='https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=INSERIR-ID-DO-BOTÃO-PERSONALIZADO' class='hq_paypal'><img src='https://www.paypalobjects.com/pt_BR/BR/i/btn/btn_donateCC_LG.gif' alt='Doe com PayPal!'></a></p></div><div style='width:45%;float:left;'><p style='margin:25px 5px;'>Ou fazer uma doação em Bitcoins:</p><p><a href='INSERIR LINK DA CARTEIRA ONLINE' class='hq_bitcoin'><img src='https://bitcoin.org/img/icons/opengraph.png' style='width:50px;height:50px;' alt='Doe em Bitcoins!'></a></p></div></div><div style='clear: both;'></div>";
+     })
+</script>
